c0f661622d5211eeb233d5f6ad4c90cf78849463
tania rosas-jimenez <rosastania@yahoo.com>
2015-04-23 18:22:55 -0700

    video 14

--- index.php
+++ index.php
@@ -33,5 +33,16 @@
 			return false;
 		});
 	}
+
+	$('.delete-button').click(function(){
+		var current_element = $(this);
+		var task_id = $(this).attr('id');
+
+		$.post('includes/delete-task.php', {id: task_id}, function(){
+		current_element.parent().fadeOut("fast", function(){
+			$(this).remove();
+		});
+	});
+});
 </script>
 </html>
