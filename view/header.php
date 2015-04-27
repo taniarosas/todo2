9f13c03b5510e87b8ba43f5b0ed19d0c0c15d5a6
tania rosas-jimenez <rosastania@yahoo.com>
2015-04-27 11:51:29 -0700

    fixed error

--- view/header.php
+++ view/header.php
@@ -3,6 +3,6 @@
     <head>
         <!--it links the css stylesheet to our php file-->
         <link rel="stylesheet" type="text/css" href="css/main.css">
-        <link rel="stylesheet" type="text/js" href="js/main.js">
+        <!--<link rel="stylesheet" type="text/js" href="js/main.js">-->
             <title></title>
    
